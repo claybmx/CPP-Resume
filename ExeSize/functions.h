23ef7ee151afd0b1c11ba3b3a2450d6c81f22c2f
claybmx <claybom24@gmail.com>
2023-09-20 22:18:56 -0300

    Adicionando a pasta ExeSize

---
+++ ExeSize/functions.h
@@ -0,0 +1,33 @@
+#pragma once
+
+static int func1() {
+	return 1;
+};
+static int func2() {
+	return 2;
+};
+static int func3() {
+	return 3;
+};
+static int func4() {
+	return 4;
+};
+static int func5() {
+	return 5;
+};/*
+static int func6() {
+	return 6;
+};
+static int func7() {
+	return 7;
+};
+static int func8() {
+	return 8;
+};
+static int func9() {
+	return 9;
+};
+static int func10() {
+	return 10;
+};
+*/
---
+++ ExeSize/main.cpp
@@ -0,0 +1,12 @@
+//#include "vld.h" // Nomemory leaks DETECTED!
+#include <iostream>
+#include "functions.h"
+using namespace std;
+
+int main() {
+
+	cout << func1() << endl;
+
+	system("pause");
+	return(0);
+}
